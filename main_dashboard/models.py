fce32327c619e6a850481c7a12da2e5b8ff2d879
opiyobrayan <67815971+opiyobrayan@users.noreply.github.com>
2022-10-11 09:33:48 +0300

    Update models.py

--- main_dashboard/models.py
+++ main_dashboard/models.py
@@ -1,5 +1,4 @@
-from email.policy import default
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
